96e31895adea853151404f2b79632e4972b3389e
Dreni956 <drendren174@gmail.com>
2024-12-19 15:11:25 +0000

    ndryshime

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,14 +1,52 @@
-console.log('hello world')
+// console.log('hello world')
 
 
-var b = 30;
-var c = 20;
-var d = b + c;
-console.log(d);
+// var b = 30;
+// var c = 20;
+// var d = b + c;
+// console.log(d);
 
-var n = 99
-n++;
-console.log(n);
+// var n = 99
+// n++;
+// console.log(n);
+
+// var o = 101;
+// o--;
+// console.log(o);
+
+// var x = 25;
+// x += 5;
+// console.log(x);
+
+
+// var y = 25;
+// y -=5;
+// console.log(y);
+
+// var p = 20;
+// var q = p ** 2;
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 == num2);
+
+var e = 100;
+var c = '4';
+console.log(e === c);
+
+var firstName = Dren 
+var lastName = Hajdari
+var fullName = firstName +' '+ lastName;
+console.log(fullName);
+
+var age = 23
+ console.log();
+ 
+
+
+
+
+
 
 
 
