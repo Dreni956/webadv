491772b66811a9dfe0125ef7724b1c82bef90687
Dreni956 <drendren174@gmail.com>
2025-01-14 16:16:30 +0100

    ndryshime

--- Javascript/lesson28.js
+++ Javascript/lesson28.js
@@ -11,7 +11,7 @@
 
 
 // button2.onmouseover = function(){
-//     alert('over')
+//     alert('over?'
 // }
 
 // button3.onmouseleave = function(){
@@ -22,10 +22,34 @@
 // }
 
 
-var v_button1 = document.getElementById ('btn1')
-var v_text = document.getElementById(txt1)
+// var v_button1 = document.getElementById ('btn1')
+// var v_text = document.getElementById('txt1')
 
-v_button1 = function() {
-    v_text.style.color = 'red'
-    v_text.style.backgroundColor = 'lightgrey'
+// v_button1.onclick = function() {
+//     // v_text.style.color = 'red'
+//     // v_text.style.backgroundColor = 'lightgrey'
+//     v_text.setAttribute('class', 'text');
+// }
+
+//  v_text.onmouseover = function () {
+//     v_text.style.cssText = 'color:red; background:lightblue; padding:20; font-size:180px;'
+
+// }
+
+var btn_circle = document.getElementById('circle');
+var btn_rectangle = document.getElementById('rectangle');
+var btn_Triangle = document.getElementById('triangle');
+
+var circle = document.getElementById('circle')
+var rect = document.getElementById('rectangle')
+var tri = document.getElementById('triangle')
+
+btn_circle.onclick = function () {
+    circle.setAttribute('class','circle_shape')
+}
+btn_rectangle.onclick = function () {
+    circle.setAttribute('class','rectangle_shape')
+}
+btn_circle.onclick = function () {
+    circle.setAttribute('class','triangle_shape')
 }
