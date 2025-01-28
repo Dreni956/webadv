c7d22cd308f179dc4cdf9625a1a3aa492e330946
Dreni956 <drendren174@gmail.com>
2025-01-28 15:11:17 +0000

    Ndryshime

---
+++ Javascript/lesson31.js
@@ -0,0 +1,10 @@
+// var person = {firstName:'Michael' + lastName: 'Jordan' + age:60} 
+// var text = ''
+// var x 
+// var (x in person)
+// console.log();
+
+var students = ['Eris', 'Ledri', 'Lisa']
+var input = document.getElementById('input')
+var btnAdd = document.getElementById('btnAdd')
+var btnShow = document.getElementById('btnShow')
