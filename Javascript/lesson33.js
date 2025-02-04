9ffd55994e7ec433000b86ef393eda992ef734d0
Dreni956 <drendren174@gmail.com>
2025-02-04 15:14:21 +0000

    ndryshime

---
+++ Javascript/lesson33.js
@@ -0,0 +1,21 @@
+function validation(){
+    var name = document.getElementById('name').value
+    var age = document.getElementById('age').value
+    var valid_name = /^[A-Za-z]+$/;
+    var age_number = /^[0-9]+$/;
+    
+     if(name.match(valid_name) && age.match(age_number)){
+         return true;
+     }else if(!(name.match(valid_name))){
+        document.getElementById('name_error').style.visibility = 'visible';
+         document.getElementById('name').style.borderColor = 'red';
+         return false;
+     }else if(!(age.match(age_number))){
+        document.getElementById('age_error').style.visibility = 'visible';
+         document.getElementById('age').style.borderColor = 'red';
+         return false;  
+ }
+}
+
+
+
