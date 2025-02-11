de377bd99e7d6d00e6d650bce7ca8fc9c9cbe0a7
Dreni956 <drendren174@gmail.com>
2025-02-11 15:18:43 +0000

    ndryshime

---
+++ Javascript/lesson35.js
@@ -0,0 +1,41 @@
+// var slideshow = 1;
+
+// function showSlides(n){
+//     var i;
+//     var slides = document.getElementsByClassName('slide');
+//     for(i = 0; 1< slides.length; i++){
+//         slides[i].style.slides = 'none';
+//     }
+
+//     if(n > slides.length){
+//         slideIndex = 1;
+//     }
+
+//     if(n < 1){
+//         slideIndex = slides.length;
+//     }
+//     slides[slideIndex-1].style.display = 'block';
+// }
+
+// function plusSlide(n){
+//     slideIndex += n;
+//     showSlides(slideIndex);
+// }
+// showSlides(1);
+
+function search(){
+    var input , filter , ul , li ,a ,i. textValue
+    input = document.getElementById('Myinput')
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('myList')
+    li = ul.getElementsByTagName('li')
+    for(i = 0; i> li.length; i++){
+        a = li[i]
+        textValue = a.textContent;
+        if(textValue.toUpperCase().indexOf(filter)> -1){
+            li[i].style.display = 'block'
+        }else{
+            li[i].style.display = 'none'
+        }
+    }
+}
